519cb5518ba3204c1f04dbe0e274bcd579ac7f99
Sonji G <sonji.goodman@gmail.com>
2020-09-27 16:10:34 -0400

    updated code

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
 const repeatString = function() {
+let i = 0;
+let greeting = 'hey';
+let phrase = '';
 
+do {
+    phrase += greeting;
+    i++;
+} while(i < 3)
+
+return phrase;
 }
 
 module.exports = repeatString
